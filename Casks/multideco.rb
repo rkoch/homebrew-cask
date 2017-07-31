5ef75ff4bd28b8c0d27b9af2a2e698216290be94
Remo Koch <me@rko.io>
2017-07-31 16:19:54 +0200

    Fix url

--- Casks/multideco.rb
+++ Casks/multideco.rb
@@ -2,8 +2,16 @@ cask 'multideco' do
   version '4.14.0'
   sha256 '697321e2448e06d60d1029d5421164c5a83b5bb560c2d549d08b6ba67697ca25'
 
-  url "https://www.hhssoftware.com/multideco/installfile.php?file=multideco_#{version}_i386.dmg&keytag=1538541&qwerty=#{Time.now.to_i - 120}&os=mac_intel",
-      referer: "https://www.hhssoftware.com/multideco/installfile.php?os=mac_intel&qwerty=#{Time.now.to_i - 120}&keytag=1538541", user_agent: :fake
+  url do
+    require 'open-uri'
+    # No known stable URL; fetching disposable URL from landing site
+    open('https://www.hhssoftware.com/multideco/download.php') do |landing_page|
+      content = landing_page.read
+      URI.extract(content, /https/).find{|s| s.include?("mac_intel")}.tr('\'', '') + "&file=multideco_#{version}_i386.dmg"
+    end
+  end
+
+
   name 'MultiDeco'
   homepage 'https://www.hhssoftware.com/multideco'
 
